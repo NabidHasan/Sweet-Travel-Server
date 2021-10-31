9f9142c73c0a018735e9ed0f575bf31470e97d1b
Nabid Hasan <nabid15-1832@diu.edu.bdgit config --global user.email nabid15-1832@diu.edu.bdgit config --global user.email nabid15-1832@diu.edu.bd>
2021-10-31 11:41:32 +0600

    initial commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
 async function run() {
     try {
         await client.connect();
