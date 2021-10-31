68274ca614631760c64ee6e7209f95e8ec894584
Nabid Hasan <nabid15-1832@diu.edu.bdgit config --global user.email nabid15-1832@diu.edu.bdgit config --global user.email nabid15-1832@diu.edu.bd>
2021-10-31 11:40:21 +0600

    removing node_modules

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = 5000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zsdqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected database")
+
+    }
+    finally {
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Running Sweet server')
+});
+
+app.listen(port, () => {
+    console.log('Running Sweet server', port);
+})
