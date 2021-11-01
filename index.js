0a3efefd4b2a53cfd117fb3b91958a4fdbcfd776
Nabid Hasan <nabid15-1832@diu.edu.bdgit config --global user.email nabid15-1832@diu.edu.bdgit config --global user.email nabid15-1832@diu.edu.bd>
2021-11-01 11:52:03 +0600

    Second commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const { MongoClient } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = 5000;
 
-
+//middleware
+app.use(cors());
+app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zsdqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,7 +16,35 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log("Connected database")
+        const database = client.db("sweetTravel");
+        const servicesCollections = database.collection("services");
+
+        const database2 = client.db("sweetTravel");
+        const servicesOrders = database2.collection("myOrder");
+
+        app.get('/services', async (req, res) => {
+
+            const cursor = servicesCollections.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.post('/services', async (req, res) => {
+            console.log(req.body)
+            const service = req.body;
+            const result = await servicesCollections.insertOne(service);
+            res.json(result)
+        });
+
+        //myOrder items
+
+        app.post('/myOrder', async (req, res) => {
+            console.log(req.body);
+            const service = req.body;
+            const result = await servicesOrders.insertOne(service);
+            res.json(result)
+        })
+
 
     }
     finally {
